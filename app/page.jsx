ed1932a881b7651d913646fef3acbe9a372f98e1
Yeshitha Dishan <yeshithadishan04@gmail.com>
2025-04-07 14:34:21 +0530

    Home Page Sections

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,16 @@
 import HeaderNav from "@/components/header-nav"; // alias import
+import FeaturedMovies from "@/components/home/featured-movies";
+import Herobanner from "@/components/home/hero-banner";
 
 // SSR - Server Side Rendered - Server Component
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <HeaderNav />
-      <main className="bg-primary h-screen">Main Section</main>
+      <main className="flex-1">
+        <Herobanner />
+        <FeaturedMovies />
+      </main>
       <footer className=" bg-amber-400 h-72">Footer Section</footer>
     </div>
   );
